81d0d25aefc37231775b4f9ce830fb7d7ce6501b
Lucy <ljbastell@me.com>
2024-02-25 17:36:36 -0700

    Profile class

---
+++ code/app/src/main/java/com/example/the_tarlords/data/users/Profile.java
@@ -0,0 +1,61 @@
+package com.example.the_tarlords.data.users;
+
+import com.example.the_tarlords.placeholder.Photo;
+
+public class Profile {
+    private User user;
+    private String firstName;
+    private String lastName;
+    private Photo profilePhoto;
+    private String phoneNum;
+    private String email;
+
+    public Profile(User user, String firstName, String lastName, Photo profilePhoto, String phoneNum, String email) {
+        this.user = user;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profilePhoto = profilePhoto;
+        this.phoneNum = phoneNum;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Photo getProfilePhoto() {
+        return profilePhoto;
+    }
+    /* how exactly to make this happen?
+    public void setAutoProfilePhoto() {
+        this.profilePhoto = generateAutoProfilePhoto();
+    }
+    */
+    public void setProfilePhoto(Photo profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
