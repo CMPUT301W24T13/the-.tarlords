aba0ddbcb56a96a5177a0f08b388ce801eb5a8a3
Lucy <ljbastell@me.com>
2024-02-25 17:28:28 -0700

    User classes and subclasses with some basic methods

---
+++ code/app/src/main/java/com/example/the_tarlords/placeholder/Alert.java
@@ -0,0 +1,5 @@
+package com.example.the_tarlords.placeholder;
+
+public class Alert {
+    //NOT ACTUAL CLASS, JUST HERE SO IT DOESN'T YELL AT ME
+}
---
+++ code/app/src/main/java/com/example/the_tarlords/placeholder/EventList.java
@@ -0,0 +1,5 @@
+package com.example.the_tarlords.placeholder;
+
+public class EventList {
+    //JUST TEMP SO IT DOESN'T YELL AT ME
+}
