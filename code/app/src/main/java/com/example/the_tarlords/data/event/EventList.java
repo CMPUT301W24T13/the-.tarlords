9bfa5fe860945b14b31e76280afac2c987582fb3
khushilad <144485850+khushilad@users.noreply.github.com>
2024-02-25 17:35:38 -0700

    Events
    Added Event and event related classes as well as 2 xml files related to events

---
+++ code/app/src/main/java/com/example/the_tarlords/data/event/EventList.java
@@ -0,0 +1,35 @@
+package com.example.the_tarlords.data.event;
+
+
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * This class keeps track of a list of event objects
+ */
+public class EventList implements AddEventFragment.AddEventDialogListener {
+
+
+    private ArrayList<Event> datalist;
+    private ListView eventList;
+    private EventListAdapter eventListAdapter;
+
+
+    @Override
+    public void addEvent(Event event) {
+        eventListAdapter.add(event);
+        eventListAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void editEvent(Event oldEvent, Event newEvent) {
+        int index = datalist.indexOf(oldEvent);
+        if (index != -1) {
+            datalist.set(index , newEvent);
+            eventListAdapter.notifyDataSetChanged();
+        }
+    }
+
+}
+
