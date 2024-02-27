885a27de9f2a8dcc819702336dbffa34332c8776
gzhang6 <gz4@ualberta.ca>
2024-02-26 23:33:04 -0700

    Update Basic QR Scan Abilities
    Using Google QR code scanning libraries

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -47,7 +47,10 @@ dependencies {
     implementation("androidx.preference:preference:1.2.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
---
+++ code/app/src/main/java/com/example/the_tarlords/data/QR/ScanQR.java
@@ -0,0 +1,14 @@
+package com.example.the_tarlords.data.QR;
+
+//https://developers.google.com/ml-kit/vision/barcode-scanning/code-scanner
+
+public class ScanQR {
+    public static void getQR() {
+        //open the camera and take a picture of a qr code
+    }
+
+    public static String readQR() {
+        //possible to compare images? possible to upload images to firebase?
+        return "1";
+    }
+}
