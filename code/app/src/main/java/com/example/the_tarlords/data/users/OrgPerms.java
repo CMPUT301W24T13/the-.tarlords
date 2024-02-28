62db8790230cd470320062c2c7d4ed780a20e962
Rimsha Memon <rimsha.memon21@gmail.com>
2024-02-27 18:07:30 -0700

    Update OrgPerms.java

--- code/app/src/main/java/com/example/the_tarlords/data/users/OrgPerms.java
+++ code/app/src/main/java/com/example/the_tarlords/data/users/OrgPerms.java
@@ -1,5 +1,10 @@
 package com.example.the_tarlords.data.users;
 
-public interface OrgPerms {
+import com.example.the_tarlords.data.event.Event;
 
+public interface OrgPerms {
+    Event createEvent(Event event);
+    QRCode createQRForCheckIns(QRCode qrCode);
+
+    QRCode reuseQRCode(QR qrCode);
 }
