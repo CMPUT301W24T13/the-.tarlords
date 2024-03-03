4ea409dde5520d08dce734c2459be23972f901eb
jluc6628 <jayden6628@gmail.com>
2024-03-03 16:54:56 -0700

    Update Alert.java

--- code/app/src/main/java/com/example/the_tarlords/data/Alert/Alert.java
+++ code/app/src/main/java/com/example/the_tarlords/data/Alert/Alert.java
@@ -3,16 +3,24 @@ package com.example.the_tarlords.data.Alert;
 import com.example.the_tarlords.data.event.Event;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Alert implements Serializable {
     private String title;
     private String message;
     private Event event;
+    private String currentDateTime;
+
 
     public Alert(String title, String message, Event event) {
         this.title = title;
         this.message = message;
         this.event = event;
+
+        LocalDateTime ldt = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        this.currentDateTime = ldt.format(formatter);
     }
 
     public String getTitle() {
@@ -25,6 +33,15 @@ public class Alert implements Serializable {
     public String getMessage() {
         return message;
     }
+
+    public String getCurrentDateTime() {
+        return currentDateTime;
+    }
+
+    public void setCurrentDateTime(String currentDateTime) {
+        this.currentDateTime = currentDateTime;
+    }
+
     public void setMessage(String message) {
         this.message = message;
     }
