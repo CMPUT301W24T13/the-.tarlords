73039f8512bc054428997ae586023abd4515dba8
jluc6628 <jayden6628@gmail.com>
2024-03-02 19:06:57 -0700

    create fragment and adapter for announcements

--- code/app/src/main/java/com/example/the_tarlords/data/announcement/Announcement.java
+++ code/app/src/main/java/com/example/the_tarlords/data/announcement/Announcement.java
@@ -1,20 +1,32 @@
 package com.example.the_tarlords.data.announcement;
 
+import android.os.Build;
+
+import com.example.the_tarlords.data.event.Event;
+
+import java.time.LocalDateTime;
+
 /**
  * This class represents announcements
  */
 public class Announcement {
     private String header;
     private String content;
+    private Event event;
+    private LocalDateTime ldt;
 
     /**
      * Constructor for the announcement object
      * @param header
      * @param content
      */
-    public Announcement(String header, String content) {
+    public Announcement(String header, String content, Event event) {
         this.header = header;
         this.content = content;
+        this.event = event;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            this.ldt = LocalDateTime.now();
+        }
     }
 
     /**
@@ -31,6 +43,38 @@ public class Announcement {
      */
     public void setHeader(String header) {
         this.header = header;
+    }
+
+    /**
+     * getter for the associated event
+     * @return event
+     */
+    public Event getEvent() {
+        return event;
+    }
+
+    /**
+     * setter for the associated event
+     * @param event
+     */
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    /**
+     * getter for local date time
+     * @return ldt
+     */
+    public LocalDateTime getLdt() {
+        return this.ldt;
+    }
+
+    /**
+     * setter for local date time
+     * @param ldt
+     */
+    public void setLdt(LocalDateTime ldt) {
+        this.ldt = ldt;
     }
 
     /**
