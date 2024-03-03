a7761ec28246fd3ac5cf9631e5fb3c2a6e0ca697
gzhang6 <144574016+gzhang6@users.noreply.github.com>
2024-03-03 01:20:54 -0700

    Update QRCode.java

--- code/app/src/main/java/com/example/the_tarlords/data/QR/QRCode.java
+++ code/app/src/main/java/com/example/the_tarlords/data/QR/QRCode.java
@@ -3,12 +3,6 @@ package com.example.the_tarlords.data.QR;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.widget.ImageView;
-<<<<<<< Updated upstream
-import android.widget.Toast;
-
-import androidx.fragment.app.FragmentActivity;
-=======
->>>>>>> Stashed changes
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
@@ -19,23 +13,7 @@ import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class QRCode {
-<<<<<<< Updated upstream
-    private static FragmentActivity activity;
-    private String QRId;
 
-    public QRCode(FragmentActivity activity) {
-        QRCode.activity = activity;
-    }
-
-    public void scanQr() {
-        IntentIntegrator intentIntegrator = new IntentIntegrator(activity);
-        intentIntegrator.setPrompt("Scan QR code");
-        intentIntegrator.setOrientationLocked(true); // Enable rotation
-        intentIntegrator.initiateScan();
-    }
-
-=======
->>>>>>> Stashed changes
     public void generateQR(String text, ImageView imageView) {
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
 
@@ -51,15 +29,4 @@ public class QRCode {
             e.printStackTrace();
         }
     }
-<<<<<<< Updated upstream
-
-    public void setQRId(String newId) {
-        QRId = newId;
-    }
-
-    public String getQRId() {
-        return QRId;
-    }
-=======
->>>>>>> Stashed changes
 }
