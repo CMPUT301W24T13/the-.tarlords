492c62d4935cc31d13defc10629279c36d73a870
gzhang6 <gz4@ualberta.ca>
2024-03-05 16:44:39 -0700

    Update TakePhotoActivity.java

--- code/app/src/main/java/com/example/the_tarlords/data/QR/TakePhotoActivity.java
+++ code/app/src/main/java/com/example/the_tarlords/data/QR/TakePhotoActivity.java
@@ -1,28 +1,14 @@
 package com.example.the_tarlords.data.QR;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.Manifest;
 import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.util.Objects;
 
