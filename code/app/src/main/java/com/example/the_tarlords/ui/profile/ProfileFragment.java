ea07aa1f2431fce7982b8453e92918a4418a3123
Lucy <124856770+gjiyun@users.noreply.github.com>
2024-03-06 14:42:17 -0700

    edited profile fragment and xml

--- code/app/src/main/java/com/example/the_tarlords/ui/profile/ProfileFragment.java
+++ code/app/src/main/java/com/example/the_tarlords/ui/profile/ProfileFragment.java
@@ -15,9 +15,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.the_tarlords.R;
+import com.example.the_tarlords.data.users.Profile;
 
 public class ProfileFragment extends Fragment {
-
+    private Profile profile;
     private ProfileViewModel mViewModel;
 
     public static ProfileFragment newInstance() {
@@ -30,10 +31,18 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         ImageView profilePhotoImageView = view.findViewById(R.id.image_view_profile);
-        EditText nameEditText = view.findViewById(R.id.edit_text_name);
+        EditText firstNameEditText = view.findViewById(R.id.edit_text_first_name);
+        EditText lastNameEditText = view.findViewById(R.id.edit_text_last_name)
         EditText phoneEditText = view.findViewById(R.id.exit_text_phone);
         EditText emailEditText = view.findViewById(R.id.edit_text_email);
-        //imageView.setImageBitmap(bitmap); lord help me jesus i fear i am on my last straw
+
+        if (profile != null) {
+            profilePhotoImageView.setImageBitmap(profile.getProfilePhoto());
+            firstNameEditText.setText(profile.getFirstName());
+            lastNameEditText.setText(profile.getLastName());
+            phoneEditText.setText(profile.getPhoneNum());
+            emailEditText.setText(profile.getEmail());
+        }
         return view;
     }
 
