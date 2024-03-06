dc83d101f557616656a705f4250676bb108282bb
jluc6628 <jayden6628@gmail.com>
2024-03-05 23:44:58 -0700

    Create Notification.java

---
+++ code/app/src/main/java/com/example/the_tarlords/data/Notification/Notification.java
@@ -0,0 +1,36 @@
+package com.example.the_tarlords.data.Notification;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.the_tarlords.R;
+
+public class Notification {
+    private Context context;
+
+    public Notification(Context context){
+        this.context = context;
+
+    }
+
+    public void sendNotification(){
+        String title = "Notification title";
+        String body = "New Notification";
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel notificationChannel = new NotificationChannel("channel","channel name",importance);
+        notificationChannel.setDescription(body);
+        NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"channel")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setPriority(importance);
+        notificationManager.createNotificationChannel(notificationChannel);
+        notificationManager.notify(1,builder.build());
+    }
+
+}
