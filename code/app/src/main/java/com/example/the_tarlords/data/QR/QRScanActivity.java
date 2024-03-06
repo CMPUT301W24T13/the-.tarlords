cdb7bccdc1d96ef6d3302beb05172436d215ee30
Rimsha Memon <rimsha.memon21@gmail.com>
2024-03-05 20:13:32 -0700

    Update QRScanActivity.java

--- code/app/src/main/java/com/example/the_tarlords/data/QR/QRScanActivity.java
+++ code/app/src/main/java/com/example/the_tarlords/data/QR/QRScanActivity.java
@@ -14,6 +14,7 @@ import androidx.core.app.ActivityCompat;
 import com.example.the_tarlords.data.attendance.Attendance;
 import com.example.the_tarlords.data.event.Event;
 import com.example.the_tarlords.data.users.Attendee;
+import com.example.the_tarlords.ui.event.EventDetailsFragment;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
