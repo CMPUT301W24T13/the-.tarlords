07ba1c0e96aa06321b6443a8d101482a9cb9b712
Isabelle Hansen-Yang <hansenyang@proton.me>
2024-03-07 12:42:27 -0600

    update

--- code/app/src/main/java/com/example/the_tarlords/MainActivity.java
+++ code/app/src/main/java/com/example/the_tarlords/MainActivity.java
@@ -86,18 +86,11 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.options_menu, menu);
         return true;
-    }
-
-    /*@Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        switch (item.getItemId()){
-            case R.id.action
-        }
     }*/
 
     @Override
