11c6fbb239b5b9f72772d0b29e780eebb99aa8ac
gzhang6 <gz4@ualberta.ca>
2024-03-22 19:59:28 -0600

    Update QRCode.java

--- code/app/src/main/java/com/example/the_tarlords/data/QR/QRCode.java
+++ code/app/src/main/java/com/example/the_tarlords/data/QR/QRCode.java
@@ -8,20 +8,10 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-
-import com.example.the_tarlords.data.event.Event;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.AggregateQuery;
-import com.google.firebase.firestore.AggregateQuerySnapshot;
-import com.google.firebase.firestore.AggregateSource;
+import com.example.the_tarlords.MainActivity;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
@@ -29,12 +19,45 @@ import com.google.zxing.common.BitMatrix;
 
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * The QRCode class provides a method to generate a QR code from a given text and display it in an ImageView.
  */
 public class QRCode {
-    private FirebaseFirestore db;
-    private CollectionReference eventsRef;
+    private String qrID;
+    private static CollectionReference QRRef = MainActivity.db.collection("QRCodes");
+
+    public String makeQR(String eventID) {
+        //randomly generate a new QRid and add to firebase
+        makeNewDocID();
+        sendToFirebase(eventID);
+        return qrID;
+    }
+
+    public void makeNewDocID() {
+        DocumentReference ref = QRRef.document(); //get new document from firestore
+        qrID = ref.getId(); //assign event id to doc id
+    }
+
+    public void sendToFirebase(String eventID) {
+        // Add the new user document to Firestore
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("QRId", qrID);
+        docData.put("EventId", eventID);
+
+        QRRef.document(qrID).set(docData)
+                .addOnSuccessListener(aVoid -> {
+                    // Document successfully added
+                    Log.d("debug", "User added successfully to Firestore");
+                })
+                .addOnFailureListener(e -> {
+                    // Handle the failure
+                    Log.e("debug", "Error adding user to Firestore", e);
+                });
+    }
+
 
     /**
      * Generates a QR code from the provided text and sets it to the specified ImageView.
@@ -76,5 +99,9 @@ public class QRCode {
         intent.setType("image/png");
         intent.putExtra(Intent.EXTRA_STREAM, uri);
         activity.startActivity(Intent.createChooser(intent, "Share"));
+    }
+
+    public String getQrID() {
+        return qrID;
     }
 }
