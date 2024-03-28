d0fc7b7bc6ead799e60c90a2df29311939e170af
Isabelle Hansen-Yang <hansenyang@proton.me>
2024-03-28 15:09:08 -0500

    fixing merge conflicts

---
+++ code/app/src/main/java/com/example/the_tarlords/data/users/Organizer.java
@@ -0,0 +1,15 @@
+package com.example.the_tarlords.data.users;
+
+//Notes for myself(Rimsha): Finish the methods once others are done their parts
+// ask lucy if she is putting event details page and event poster together or not. If they are separate, then create one more method.
+/* ask iz if she is working on attendeeSignUpList(). And for the setLimitOnSignUps() thing, can use the reachedMaxCap() method from event
+   in SignUp() method from Attendance to check if we have reached max, there is also maxLimit org can set */
+
+/**
+ * This is the Organizer Class, which is subclass of Attendee but with more permissions
+ */
+
+public class Organizer {
+
+
+}
