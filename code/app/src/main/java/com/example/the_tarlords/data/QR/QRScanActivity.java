0a065641e5610a5716559a32ac9da366ec85ba68
gzhang6 <gz4@ualberta.ca>
2024-03-30 13:43:19 -0600

    reuse

--- code/app/src/main/java/com/example/the_tarlords/data/QR/QRScanActivity.java
+++ code/app/src/main/java/com/example/the_tarlords/data/QR/QRScanActivity.java
@@ -147,6 +147,7 @@ public class QRScanActivity extends AppCompatActivity {
                 }
             }
         });
+        Toast.makeText(this, "Invalid QR", Toast.LENGTH_SHORT).show();
     }
 
     @Override
