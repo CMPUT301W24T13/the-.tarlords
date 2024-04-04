af3502d9a3f49dd74dd1f3d5721c0bfe53cfbf6c
iz <158203889+br0ken-mirrorz@users.noreply.github.com>
2024-04-03 21:40:40 -0500

    Delete code/app/src/main/java/com/example/the_tarlords/data/attendance/AttendanceCallback.java

--- code/app/src/main/java/com/example/the_tarlords/data/attendance/AttendanceCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.the_tarlords.data.attendance;
-
-import com.example.the_tarlords.data.users.Attendee;
-
-import java.util.ArrayList;
-
-public interface AttendanceListCallback {
-    void onAttendanceLoaded(ArrayList<Attendee> attendanceList);
-}
