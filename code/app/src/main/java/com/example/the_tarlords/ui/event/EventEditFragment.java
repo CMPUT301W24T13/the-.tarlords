4b120e40540843ddb4c8f9112181096f408b2b1c
lucy bastell <ljybastell@gmail.com>
2024-04-07 14:00:15 -0600

    deleted a random import that wasn't being used

--- code/app/src/main/java/com/example/the_tarlords/ui/event/EventEditFragment.java
+++ code/app/src/main/java/com/example/the_tarlords/ui/event/EventEditFragment.java
@@ -11,7 +11,6 @@ import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
